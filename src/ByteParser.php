2ecd3723d4a4f74b7d8230e789acbf7acf22ede0
A.B. Carroll <ben@hl9.net>
2020-06-03 08:55:26 -0400

    feature(parser): New ByteParser, which matches bytes.
    This is actually incredibly similar to the LookaheadParser, however was
    written independently before I had knowledge of the existance of it.
    
    There are small behavioural differences, but they are otherwise equiv.

---
+++ src/ByteParser.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Ferno\Loco;
+
+/**
+ * ByteParser consumes all bytes until a particular byte sequence is reached, or fail if that sequence never occurs.
+ *
+ * For example, a ByteParser(["a", "b"]) on input "zzzz\x00a", assuming the \x00 is interpreted as NULL byte, will
+ * return back to the caller "zzzz\x00", offset +5, success -- next byte on chunk "a".
+ *
+ * If you are matching text, you may wish to look at the Utf8Parser which exhibits the precise same behaviour, however
+ * is UTF-8 aware as the name implies.  This ByteParser is significantly more performant, of course, not needing to
+ * validate the UTF-8.
+ *
+ * @author A.B. Carroll <ben@hl9.net>
+ */
+class ByteParser
+{
+    private $matchExceptList = [];
+
+    public function __construct($stopOnCharactersBytes, $callback = null)
+    {
+        $this->matchExceptList = array_combine($stopOnCharactersBytes, $stopOnCharactersBytes);
+        parent::__construct($callback);
+    }
+
+    public function getResult($string, $offset = 0): array
+    {
+        $inputMaxLen = strlen($string);
+
+        for ($x = $offset; $x < $inputMaxLen && !isset($this->matchExceptList[$string[$x]]); $x++);
+
+        if ($x === $offset) {
+            throw new ParseFailureException(
+                $this . " could not find string terminator: " . var_export($this->matchExceptList, true), $offset, $string
+            );
+        }
+
+        return [
+            "pos"  => $x,
+            "args" => $string
+        ];
+    }
+
+    /**
+     * default callback: just return the string that was matched
+     * @return array
+     */
+    public function defaultCallback(): array
+    {
+        return func_get_arg(0);
+    }
+
+    public function evaluateNullability(): bool
+    {
+        return false;
+    }
+
+}
