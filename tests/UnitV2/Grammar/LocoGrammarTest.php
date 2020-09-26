43523a1ed64522e65bb70ca9a90f86664c0cfe7e
A.B. Carroll <ben@hl9.net>
2020-09-26 05:09:52 -0400

    style(tests): Minor clean-up in Loco grammar test

--- tests/UnitV2/Grammar/LocoGrammarTest.php
+++ tests/UnitV2/Grammar/LocoGrammarTest.php
@@ -3,7 +3,7 @@
 namespace Ferno\Tests\Loco\Grammar;
 
 use Ab\LocoX\Grammar\LocoGrammar;
-use Ab\LocoX\ParseFailureException;
+use Ab\LocoX\Exception\ParseFailureException;
 use PHPUnit\Framework\TestCase;
 
 class LocoGrammarTest extends TestCase
@@ -125,24 +125,24 @@ class LocoGrammarTest extends TestCase
         // new S(array("a", "a", ...))
         // new S(new APlus("a", "a", ...))
         $grammar1 = $this->grammar->parse(" S ::= 'a'+ ");
-        $grammar4 = $this->grammar->parse(" S ::= 'a' 'a' 'a' | 'a' 'a' | 'a' ");
-        $grammar2 = $this->grammar->parse(" S ::= ( 'a' 'a' 'a' | 'a' 'a' | 'a' ) ");
-        $grammar3 = $this->grammar->parse(" S ::= APlus \n APlus ::= 'a' 'a' 'a' | 'a' 'a' | 'a' ");
-
-        dump($grammar1->parse("aaa"));
-
         $this->assertEquals(array("a", "a", "a"), $grammar1->parse("aaa"));
-        $this->assertEquals(array("a", "a", "a"), $grammar4->parse("aaa"));
-        $this->assertEquals(array(array("a", "a", "a")), $grammar2->parse("aaa"));
-        $this->assertEquals(array(array("a", "a", "a")), $grammar3->parse("aaa"));
         $this->assertEquals(array("a", "a"), $grammar1->parse("aa"));
-        $this->assertEquals(array("a", "a"), $grammar4->parse("aa"));
-        $this->assertEquals(array(array("a", "a")), $grammar2->parse("aa"));
-        $this->assertEquals(array(array("a", "a")), $grammar3->parse("aa"));
         $this->assertEquals(array("a"), $grammar1->parse("a"));
-        $this->assertEquals(array("a"), $grammar4->parse("a"));
+
+        $grammar2 = $this->grammar->parse(" S ::= ( 'a' 'a' 'a' | 'a' 'a' | 'a' ) ");
+        $this->assertEquals(array(array("a", "a", "a")), $grammar2->parse("aaa"));
+        $this->assertEquals(array(array("a", "a")), $grammar2->parse("aa"));
         $this->assertEquals(array(array("a")), $grammar2->parse("a"));
+
+        $grammar3 = $this->grammar->parse(" S ::= APlus \n APlus ::= 'a' 'a' 'a' | 'a' 'a' | 'a' ");
+        $this->assertEquals(array(array("a", "a", "a")), $grammar3->parse("aaa"));
+        $this->assertEquals(array(array("a", "a")), $grammar3->parse("aa"));
         $this->assertEquals(array(array("a")), $grammar3->parse("a"));
+
+        $grammar4 = $this->grammar->parse(" S ::= 'a' 'a' 'a' | 'a' 'a' | 'a' ");
+        $this->assertEquals(array("a", "a", "a"), $grammar4->parse("aaa"));
+        $this->assertEquals(array("a", "a"), $grammar4->parse("aa"));
+        $this->assertEquals(array("a"), $grammar4->parse("a"));
 
         try {
             $grammar1->parse("");
